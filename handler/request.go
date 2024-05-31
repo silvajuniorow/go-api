c0659446f57a06543c45cf62f3eee9ac567cd45d
Junior Silva <silvajuniorow@gmail.com>
2024-05-30 22:10:08 -0300

    feat: add update opening handler logic

--- handler/request.go
+++ handler/request.go
@@ -40,3 +40,22 @@ func (r *CreateOpeningRequest) Validade() error {
 	}
 	return nil
 }
+
+type UpdateOpeningRequest struct {
+	Role     string `json:"role"`
+	Company  string `json:"company"`
+	Location string `json:"location"`
+	Remote   *bool  `json:"remote"`
+	Link     string `json:"link"`
+	Salary   int64  `json:"salary"`
+}
+
+func (r *UpdateOpeningRequest) Validade() error {
+	// if any field is provided, validate is truthy
+	if r.Role != "" || r.Company != "" || r.Location != "" || r.Remote != nil || r.Link != "" || r.Salary > 0 {
+		return nil
+	}
+
+	// if none of the fields were provided, return falsy
+	return fmt.Errorf("at least one valid field must be provided")
+}
